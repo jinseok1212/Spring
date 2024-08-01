561e97a802907aba3b5150a7a8779d96f77f240b
jinseok1212 <dlwlstjr110@naver.com>
2024-08-01 17:53:58 +0900

    first commit

---
+++ SpringDI/src/main/java/ex02/constructor/Hotel.java
@@ -0,0 +1,18 @@
+package ex02.constructor;
+
+public class Hotel {
+
+	private Chef chef;
+	
+	//호텔은 생성될 때 쉐프객체를 받는다(hotel은 chef에 의존적이다)
+	public Hotel(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public Chef getChef() {
+		return chef;
+	}
+	public Chef setChef() {
+		return chef;
+	}
+}
---
+++ SpringDI/src/main/java/ex03/autowired/Document.java
@@ -0,0 +1,6 @@
+package ex03.autowired;
+
+public class Document {
+	
+	public String data = "hello world";
+}
